fa28e7231f45830c7aecd9ed438448a2cb585325
zhangxingang <32584932@qq.com>
2019-03-26 10:26:11 +0800

    添加车道组与道路关联关系检查

--- modules/checkcore/src/storage/CheckErrorOutput.cpp
+++ modules/checkcore/src/storage/CheckErrorOutput.cpp
@@ -93,7 +93,7 @@ namespace kd {
             string ret = LEVEL_WARNING;
             if (check_model == "KXS-01-002" || check_model == "KXS-01-003" || check_model == "KXS-01-004" ||
                 check_model == "KXS-01-008" || check_model == "KXS-01-015" || check_model == "KXS-01-020" ||
-                check_model == "KXS-01-021" || check_model == "KXS-01-022") {
+                check_model == "KXS-01-021" || check_model == "KXS-01-022" || check_model == "KXS-01-023") {
                 ret = LEVEL_ERROR;
             } else if (check_model == "KXS-03-002" || check_model == "KXS-03-004" || check_model == "KXS-03-005" ||
                        check_model == "KXS-03-006" || check_model == "KXS-03-011" || check_model == "KXS-03-012" ||

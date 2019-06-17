d14021f65cd2e0675a9de759487877683271df02
zhangxingang <32584932@qq.com>
2019-06-17 15:33:24 +0800

    拆分lanegroupcheck

--- modules/checkcore/include/businesscheck/LaneGroupCheck.h
+++ modules/checkcore/include/businesscheck/LaneGroupCheck.h
@@ -1,5 +1,5 @@
 //
-// Created by zhangxingang on 19-1-17.
+// Created by zhangxingang on 19-6-17.
 //
 
 #ifndef AUTOHDMAP_DATACHECK_LANEGROUPCHECK_H
@@ -8,34 +8,9 @@
 #include "IMapProcessor.h"
 namespace kd {
     namespace dc {
-        struct LGNodeIndex{
-            LGNodeIndex() = default;
-            LGNodeIndex(const string &lg_id, const string &road_id, long f_index, long t_index) {
-                this->lanegroup_id = lg_id;
-                this->f_idx = f_index;
-                this->t_idx = t_index;
-                this->road_id = road_id;
-            }
-
-            bool operator<(const LGNodeIndex &other) {
-                return this->f_idx < other.f_idx;
-            }
-
-            bool operator>(const LGNodeIndex &other) {
-                return this->f_idx > other.f_idx;
-            }
-
-            string lanegroup_id;
-            string road_id;
-            long f_idx{};
-            long t_idx{};
-        };
-
-        /**
-         * 车道组检查
-         */
         class LaneGroupCheck : public IMapProcessor {
         public:
+            ~LaneGroupCheck() override;
 
             string getId() override;
 
@@ -43,45 +18,15 @@ namespace kd {
 
         private:
             /**
-             * 释放资源
-             * @param mapDataManager
-             */
-            void release(shared_ptr<MapDataManager> mapDataManager);
-
-            /**
-             * 车道组关联道路范围检查
-             * @return
-             */
-            void check_lanegroup_road(shared_ptr<MapDataManager> mapDataManager,
-                                  shared_ptr<CheckErrorOutput> errorOutput);
-
-            /**
              * 车道组与DIVIDER关系检查
-             * @param mapDataManager
-             * @param errorOutput
              */
-            void check_lanegroup_divider(shared_ptr<MapDataManager> mapDataManager,
-                                         shared_ptr<CheckErrorOutput> errorOutput);
+            void check_lanegroup_divider();
 
             /**
              * 车道组内的divider检查
-             * @param mapDataManager
-             * @param errorOutput
              */
-            void check_divider(shared_ptr<MapDataManager> mapDataManager,
-                               shared_ptr<CheckErrorOutput> errorOutput);
+            void check_divider();
 
-        private:
-            /**
-             * 车道组关联道路索引点详细检查逻辑
-             * @param errorOutput
-             */
-            void check_road_node_index(vector<LGNodeIndex> lg_node_index_vec, shared_ptr<DCRoad> ptr_road,
-                                       bool is_positive, shared_ptr<MapDataManager> mapDataManager,
-                                       shared_ptr<CheckErrorOutput> errorOutput);
-
-            void check_index_fill_all(vector<LGNodeIndex> lg_node_index_vec, shared_ptr<DCRoad> ptr_road,
-                                      bool is_positive, shared_ptr<CheckErrorOutput> errorOutput);
 
             /**
              * 车道组编号检查
@@ -102,11 +47,11 @@ namespace kd {
              * @param lane_group
              * @param ptr_dividers
              */
-            void check_divider_length(shared_ptr<MapDataManager> mapDataManager, shared_ptr<CheckErrorOutput> errorOutput,
-                                      const string &lane_group, const vector<shared_ptr<DCDivider>> &ptr_dividers);
-
+            void check_divider_length(const string &lane_group, const vector<shared_ptr<DCDivider>> &ptr_dividers);
         private:
-            const string id = "lanegroup_check";
+            string id_ = "lane_group_check";
+            shared_ptr<MapDataManager> data_manager_;
+            shared_ptr<CheckErrorOutput> error_output_;
         };
     }
 }
---
+++ modules/checkcore/include/businesscheck/LaneGroupRelationCheck.h
@@ -0,0 +1,77 @@
+//
+// Created by zhangxingang on 19-1-17.
+//
+
+#ifndef AUTOHDMAP_DATACHECK_LaneGroupRelationCheck_H
+#define AUTOHDMAP_DATACHECK_LaneGroupRelationCheck_H
+
+#include "IMapProcessor.h"
+namespace kd {
+    namespace dc {
+        struct LGNodeIndex{
+            LGNodeIndex() = default;
+            LGNodeIndex(const string &lg_id, const string &road_id, long f_index, long t_index) {
+                this->lanegroup_id = lg_id;
+                this->f_idx = f_index;
+                this->t_idx = t_index;
+                this->road_id = road_id;
+            }
+
+            bool operator<(const LGNodeIndex &other) {
+                return this->f_idx < other.f_idx;
+            }
+
+            bool operator>(const LGNodeIndex &other) {
+                return this->f_idx > other.f_idx;
+            }
+
+            string lanegroup_id;
+            string road_id;
+            long f_idx{};
+            long t_idx{};
+        };
+
+        /**
+         * 车道组检查
+         */
+        class LaneGroupRelationCheck : public IMapProcessor {
+        public:
+
+            string getId() override;
+
+            bool execute(shared_ptr<MapDataManager> mapDataManager, shared_ptr<CheckErrorOutput> errorOutput) override;
+
+        private:
+            /**
+             * 释放资源
+             * @param mapDataManager
+             */
+            void release(shared_ptr<MapDataManager> mapDataManager);
+
+            /**
+             * 车道组关联道路范围检查
+             * @return
+             */
+            void check_lanegroup_road(shared_ptr<MapDataManager> mapDataManager,
+                                  shared_ptr<CheckErrorOutput> errorOutput);
+
+        private:
+            /**
+             * 车道组关联道路索引点详细检查逻辑
+             * @param errorOutput
+             */
+            void check_road_node_index(vector<LGNodeIndex> lg_node_index_vec, shared_ptr<DCRoad> ptr_road,
+                                       bool is_positive, shared_ptr<MapDataManager> mapDataManager,
+                                       shared_ptr<CheckErrorOutput> errorOutput);
+
+            void check_index_fill_all(vector<LGNodeIndex> lg_node_index_vec, shared_ptr<DCRoad> ptr_road,
+                                      bool is_positive, shared_ptr<CheckErrorOutput> errorOutput);
+
+        private:
+            const string id = "lanegroup_check";
+        };
+    }
+}
+
+
+#endif //AUTOHDMAP_DATACHECK_LaneGroupRelationCheck_H

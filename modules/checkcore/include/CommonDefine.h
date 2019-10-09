05cf7313a11071ae2ce215f26be2efcbc6326db3
wangchangbao <wangchangbao@kuandeng.com>
2019-10-09 18:09:36 +0800

    add check item and desc

--- modules/checkcore/include/CommonDefine.h
+++ modules/checkcore/include/CommonDefine.h
@@ -184,6 +184,7 @@ namespace kd {
         const static std::string CHECK_ITEM_KXS_ROAD_006 = "KXS-04-006";
         const static std::string CHECK_ITEM_KXS_ROAD_007 = "KXS-04-007";
         const static std::string CHECK_ITEM_KXS_ROAD_008 = "KXS-04-008";
+        const static std::string CHECK_ITEM_KXS_ROAD_009 = "KXS-04-009";
 
         const static std::string CHECK_ITEM_KXS_ROAD_001_DESC = "若车道组之间连通，则其内部的道路必然连通";
         const static std::string CHECK_ITEM_KXS_ROAD_002_DESC = "道路与车道组的两个车道边缘线应无交叉点。双向道路除外";
@@ -193,7 +194,7 @@ namespace kd {
         const static std::string CHECK_ITEM_KXS_ROAD_006_DESC = "道路前后结点重复,前后点xy坐标相同（z值可能相同）";
         const static std::string CHECK_ITEM_KXS_ROAD_007_DESC = "道路结点出现拐点，或者角度过大";
         const static std::string CHECK_ITEM_KXS_ROAD_008_DESC = "道路形态点间距过近";
-
+        const static std::string CHECK_ITEM_KXS_ROAD_009_DESC = "综合交叉点道路拓扑关系完备性检查";
 
         // 车道中心线
         const static std::string CHECK_ITEM_KXS_LANE_001 = "KXS-05-001";
